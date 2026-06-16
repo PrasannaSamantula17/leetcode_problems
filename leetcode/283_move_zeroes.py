b4a3de193a9e3b25bdc291568a25ec869f26a1d1
Prasanna <gowriprasanna3117@gmail.com>
2026-06-16 12:08:36 +0530

    Create 283_move_zeroes.py

---
+++ leetcode/283_move_zeroes.py
@@ -0,0 +1,14 @@
+# Time Complexity - O(n)
+# Space Complexity - O(1)
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        slow = 0
+        for fast in range(0,len(nums)):
+            if nums[fast]!=0:
+                nums[slow],nums[fast] = nums[fast],nums[slow]
+                slow += 1
+        return nums
